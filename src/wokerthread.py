7145215f37ffabde6c95912fd0c1ecf6636173cb
tkhs1121 <tkhs1121@protonmail.com>
2022-02-07 00:53:20 +0900

    動的にHTMLを生成する（時間表示）

--- src/wokerthread.py
+++ src/wokerthread.py
@@ -1,7 +1,8 @@
 import os
+import textwrap
 import traceback
 from socket import socket
-from typing import Tuple
+from typing import Tuple, Optional
 from threading import Thread
 from datetime import datetime
 
@@ -33,16 +34,36 @@ class WorkerThread(Thread):
 
             method, path, http_version, request_header, request_body = self.parse_http_request(request)
 
-            try:
-                response_body = self.get_static_file_content(path)
+            response_body: bytes
+            content_type: Optional[str]
+            rensponse_line: str
 
-                response_line = "HTTP/1.1 200 OK \r\n"
+            if path == "/now":
+                html = f"""
+                        <html>
+                        <body>
+                            <h1>Now: {datetime.now()}</h1>
+                        </body>
+                        </html>
+                        """
+                response_body = textwrap.dedent(html).encode()
 
-            except OSError:
-                response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
-                response_line = "HTTP/1.1 404 Found\r\n"
-        
-            response_header = self.build_response_header(path, response_body)
+                content_type = "text/html"
+                response_line = "HTTP/1.1 200 OK\r\n"
+            else:
+                try:
+                    response_body = self.get_static_file_content(path)
+
+                    content_type = None
+
+                    response_line = "HTTP/1.1 200 OK \r\n"
+
+                except OSError:
+                    response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
+                    content_type = "text/html"
+                    response_line = "HTTP/1.1 404 Found\r\n"
+            
+            response_header = self.build_response_header(path, response_body, content_type)
 
             response = (response_line + response_header + "\r\n").encode() + response_body
 
@@ -74,14 +95,15 @@ class WorkerThread(Thread):
         with open(static_file_path, "rb") as f:
             return f.read()
 
-    def build_response_header(self, path: str, response_body: bytes) -> str:
+    def build_response_header(self, path: str, response_body: bytes, content_type: Optional[str]) -> str:
 
-        if "." in path:
-            ext = path.rsplit(".", maxsplit=1)[-1]
-        else:
-            ext = ""
+        if content_type is None:
+            if "." in path:
+                ext = path.rsplit(".", maxsplit=1)[-1]
+            else:
+                ext = ""
 
-        content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
+            content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
     
         response_header = ""
         response_header += f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M %S GMT')}\r\n"
