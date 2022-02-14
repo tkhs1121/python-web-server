6db4b0e5b0d439c6d34ca986787680a7d4d6728b
tkhs1121 <tkhs1121@protonmail.com>
2022-02-14 17:16:00 +0900

    viewsにロジックに分離

--- src/wokerthread.py
+++ src/wokerthread.py
@@ -9,6 +9,8 @@ from typing import Tuple, Optional
 from threading import Thread
 from datetime import datetime
 
+import views
+
 class WorkerThread(Thread):
 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -42,57 +44,15 @@ class WorkerThread(Thread):
             rensponse_line: str
 
             if path == "/now":
-                html = f"""
-                        <html>
-                        <body>
-                            <h1>Now: {datetime.now()}</h1>
-                        </body>
-                        </html>
-                        """
-
-                response_body = textwrap.dedent(html).encode()
-                content_type = "text/html; charset=UTF-8"
-                response_line = "HTTP/1.1 200 OK\r\n"
+                response_body, content_type, response_line = views.now()
 
             elif path == "/show_request":
-                html = f"""
-                        <html>
-                        <body>
-                            <h1>Request Line:</h1>
-                            <p>
-                                {method} {path} {http_version}
-                            </p>
-                            <h1>Header:</h1>
-                            <pre>{pformat(request_header)}</pre>
-                            <h1>Body</h1>
-                            <pre>{request_body.decode("utf-8", "ignore")}</pre>
-                        </body>
-                        </html>
-                """
-
-                response_body = textwrap.dedent(html).encode()
-                content_type = "text/html; charset=UTF-8"
-                response_line = "HTTP/1.1 200 OK\r\n"
-
+                response_body, content_type, response_line = views.show_request(
+                    method, path, http_version, request_header, request_body
+                )
+                
             elif path == "/parameters": 
-                if method == "GET":
-                    response_body = b"<html><body><h1>405 Method Not Allowed</h1></body></html>"
-                    content_type = "text/html; charset=UTF-8"
-                    response_line = "HTTP/1.1 405 Method Not Allowed\r\n"
-
-                elif method == "POST":
-                    post_params = urllib.parse.parse_qs(request_body.decode())
-                    html = f"""\
-                        <html>
-                        <body>
-                            <h1>Parameters:</h1>
-                            <pre>{pformat(post_params)}</pre>
-                        </body>
-                        </html>
-                    """
-                    response_body = textwrap.dedent(html).encode()
-                    content_type = "text/html; charset=UTF-8"
-                    response_line = "HTTP/1.1 200 OK\r\n"
+                response_body, content_type, response_line = views.parameters(method, request_body)
 
             else:
                 try:
