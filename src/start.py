d68cf314eb0b91204648877688c49c85266cb3f8
tkhs1121 <tkhs1121@protonmail.com>
2022-02-15 11:37:21 +0900

    サーバー起動スクリプトを分離

---
+++ src/start.py
@@ -0,0 +1,5 @@
+from webserver import WebServer
+
+if __name__ == '__main__':
+    server = WebServer()
+    server.serve()
--- src/webserver.py
+++ src/webserver.py
@@ -14,7 +14,7 @@ class WebServer:
                 print('=== Server: クライアントからの接続を待ちます===')
                 (client_socket, address) = server_socket.accept()
                 print(f'=== Server: クライアントの接続が完了しました remote_address: {address} ===')
-  
+
                 thread = WorkerThread(client_socket, address)
 
                 thread.start()
@@ -30,7 +30,3 @@ class WebServer:
         server_socket.listen(10)
 
         return server_socket
-    
-if __name__ == '__main__':
-    server = WebServer()
-    server.serve()
